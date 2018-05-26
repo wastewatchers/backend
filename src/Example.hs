a7d1347b0026242326b956ba6c3f77a2f92a4977
Tobias Markus <tobias@miglix.eu>
2018-05-26 19:05:25 +0200

    Update server

--- src/Example.hs
+++ src/Example.hs
@@ -1,20 +1,76 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
-module Example (runApp, app) where
+module Example (runApp) where
 
 import           Data.Aeson (Value(..), object, (.=))
 import           Network.Wai (Application)
 import qualified Web.Scotty as S
 
-app' :: S.ScottyM ()
-app' = do
+--import Database.PostgreSQL.Simple
+import Hasql.Connection
+import Hasql.Session
+import qualified Hasql.Encoders as E
+import qualified Hasql.Decoders as D
+--import Hasql.Statement
+import Hasql.Query
+
+import Data.Text (Text)
+import qualified Data.Text.Lazy as T
+
+import Data.UUID
+import Data.Functor.Contravariant
+import Data.Monoid ((<>))
+
+import Control.Monad.Trans.Class
+
+data User = User {
+  userId :: UUID,
+  username :: Text
+  } deriving (Eq, Show)
+
+data Product = Product {
+  productId :: Text,
+--  asin :: Maybe Text,
+  name :: Text,
+  manufacturer :: Maybe Text
+  } deriving (Eq, Show)
+-- addProduct(id: EAN13,name: text, manufacturer: text)
+prod :: E.Params Product
+prod =
+     contramap productId (E.value E.text)
+  <> contramap name (E.value E.text)
+  <> contramap manufacturer (E.nullableValue E.text)
+
+  --  <> contramap asin (nullableParam text)
+cfg :: Settings
+cfg = settings "localhost" 5432 "tobias" "" "wastewatchers"
+
+rdb c = do
+  (Right _) <- flip run c $ sql "select 1+1"
+  release c
+
+
+app' :: Connection -> S.ScottyM ()
+app' conn = do
+  S.put "/product/:id" $ do
+    ean <- S.param "id"
+    name <- S.param "name"
+    manufacturer <- S.param "manufacturer"
+    let sq = "insert into products (id, name, manufacturer) values ($1, $2, $3)"
+        st = statement sq prod D.unit True
+    res <- lift $ flip run conn $ query (Product ean name (Just manufacturer)) st
+    case res of
+      Left err -> S.raise . T.pack . show $ err
+      Right _ -> S.text "test"
+
   S.get "/" $ do
     S.text "hello"
 
   S.get "/some-json" $ do
     S.json $ object ["foo" .= Number 23, "bar" .= Number 42]
 
-app :: IO Application
-app = S.scottyApp app'
-
 runApp :: IO ()
-runApp = S.scotty 8080 app'
+runApp = do
+  ac <- acquire cfg
+  case ac of
+    Left err -> error $ show err
+    Right c -> S.scotty 8080 (app' c)
