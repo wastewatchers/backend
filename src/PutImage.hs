3cafa48369088a81298795a99c413e4c1320da77
Tobias Markus <tobias@miglix.eu>
2018-05-27 10:31:59 +0200

    Fix wrong column order

--- src/PutImage.hs
+++ src/PutImage.hs
@@ -27,7 +27,7 @@ putImage conn =
     uuid <- lift $ randomIO
     productid <- S.param "productid"
     imagedata <- BS.toStrict <$> S.body
-    let sq = "insert into product_images (id, productid, image_data) values ($1, $2, $3)"
+    let sq = "insert into product_images (id, image_data, productid) values ($1, $2, $3)"
         st = statement sq imageP D.unit True
         im = Image uuid imagedata productid
     res <- lift $ run (query im st) conn
