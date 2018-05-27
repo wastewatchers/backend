7764fcc4144705033503245ea9914943184af4e1
Tobias Markus <tobias@miglix.eu>
2018-05-27 11:36:13 +0200

    Simplify query

--- src/GetRatingSummary.hs
+++ src/GetRatingSummary.hs
@@ -15,6 +15,7 @@ import Data.Aeson
 
 -- import Data.Time.Clock
 -- import Data.UUID
+import Control.Monad
 import Data.Maybe
 import Data.Text.Encoding as TE
 import qualified Hasql.Encoders as E
@@ -31,30 +32,24 @@ data Summary = Summary {
 
 summaryR = (,,,,) <$> D.value D.text <*> D.value D.text <*> D.value D.text <*> D.value D.float8 <*> D.value D.float8
 
-getSummaryS = statement sq ienc (D.singleRow summaryR) True
+getSummaryS = statement sq (E.value E.text) (D.singleRow summaryR) True
     where
         sq = "select p.name, max(r.recyclable), max(r.pl_type), avg(r.grade), avg(r.pl_weight) from ratings r, products p where p.id = r.productid and r.productid = $1 group by p.id"
-        ienc = E.value E.text
 
-getImagesS = statement sq ienc (D.rowsList (D.value D.uuid)) True
+getImagesS = statement sq (E.value E.text) (D.rowsList (D.value D.uuid)) True
     where
         sq = "select i.id from products p, product_images i where p.id = i.productid and p.id = $1"
-        ienc = E.value E.text
 
 getRatingSummary :: Connection -> S.ScottyM ()
-getRatingSummary conn =
-  S.get "/rating/:id/summary" $ do
+getRatingSummary conn = S.get "/rating/:id/summary" $ do
     ean <- S.param "id"
-    res1 <- lift $ run (query (ean :: Text) getSummaryS) conn
-    summ <- case res1 of
-      Left err -> S.raise . T.pack . show $ err
-      Right val -> pure val
-    res2 <- lift $ run (query (ean :: Text) getImagesS) conn
-    imgs <- case res2 of
-      Left err -> S.raise . T.pack . show $ err
-      Right val -> pure val
-    let (nm, re, pt, ag, aw) = summ
+    (nm, re, pt, ag, aw) <- runQuery ean getSummaryS 
+    imgs <- runQuery ean getImagesS
     S.json $ object [
         "name" .= nm, "recyclable" .= re,
         "images" .= imgs, "plastic_type" .= pt,
         "average_grade" .= ag, "average_weight" .= aw ]
+  where
+    handleResult = either (S.raise . T.pack . show) pure
+    runQuery ean st = ((lift . run (query ean st)) >=> handleResult) conn
+
