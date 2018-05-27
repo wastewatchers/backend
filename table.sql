fa260940de76f3beb389d40f066903d17a7782da
Tobias Markus <tobias@miglix.eu>
2018-05-27 10:32:20 +0200

    Fix table schema

--- table.sql
+++ table.sql
@@ -6,13 +6,13 @@ drop table products;
 --drop type recyclability;
 --create type recyclability as enum ('non_recyclable', 'recyclable', 'compostable');
 
-create table users(
-    id uuid primary key,
-    username text unique not null
-);
+--create table users(
+--    id uuid primary key,
+--    username text unique not null
+--);
 
 create table products(
-    id char(13) primary key,
+    id varchar(40) primary key,
 --    asin char(10),
     name text not null,
     manufacturer text
@@ -25,13 +25,13 @@ create table product_images(
 );
 
 create table ratings(
-    userid uuid references users(id),
-    productid char(13) references products(id),
-    grade integer constraint grade_in_range check(grade >= 1 and grade <= 5),
+    userid uuid not null,
+    productid varchar(40) references products(id),
+    grade integer not null constraint grade_in_range check(grade >= 1 and grade <= 5),
     vendor text,
-    posted timestamp with time zone,
-    pl_type text,
-    pl_weight integer,
+    posted timestamp with time zone not null,
+    pl_type text not null,
+    pl_weight integer not null,
     recyclable text not null
 --    recyclable recyclability not null
 );
