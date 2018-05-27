8cb3aac2e33336929ed20fea1f3266c4d1eb9c28
Tobias Markus <tobias@miglix.eu>
2018-05-27 11:47:03 +0200

    Improve summary

--- src/GetRatingSummary.hs
+++ src/GetRatingSummary.hs
@@ -22,19 +22,11 @@ import qualified Hasql.Encoders as E
 import Data.Text (Text)
 import Data.Functor.Contravariant
 
-data Summary = Summary {
-    name :: Text,
-    recyclable :: Recyclability,
-    plasticType :: Text,
-    avgGrade :: Double,
-    avgWeight :: Double
-    } deriving (Eq, Show)
-
-summaryR = (,,,,) <$> D.value D.text <*> D.value D.text <*> D.value D.text <*> D.value D.float8 <*> D.value D.float8
+summaryR = (,,,,,) <$> D.value D.text <*> D.value D.text <*> D.value D.text <*> D.value D.text <*> D.value D.float8 <*> D.value D.float8
 
 getSummaryS = statement sq (E.value E.text) (D.singleRow summaryR) True
     where
-        sq = "select p.name, max(r.recyclable), max(r.pl_type), avg(r.grade), avg(r.pl_weight) from ratings r, products p where p.id = r.productid and r.productid = $1 group by p.id"
+        sq = "select p.name, p.vendor, max(r.recyclable), max(r.pl_type), avg(r.grade), avg(r.pl_weight) from ratings r, products p where p.id = r.productid and r.productid = $1 group by p.id having r.pl_weight <> 0"
 
 getImagesS = statement sq (E.value E.text) (D.rowsList (D.value D.uuid)) True
     where
@@ -43,13 +35,16 @@ getImagesS = statement sq (E.value E.text) (D.rowsList (D.value D.uuid)) True
 getRatingSummary :: Connection -> S.ScottyM ()
 getRatingSummary conn = S.get "/rating/:id/summary" $ do
     ean <- S.param "id"
-    (nm, re, pt, ag, aw) <- runQuery ean getSummaryS 
-    imgs <- runQuery ean getImagesS
-    S.json $ object [
-        "name" .= nm, "recyclable" .= re,
-        "images" .= imgs, "plastic_type" .= pt,
-        "average_grade" .= ag, "average_weight" .= aw ]
+    obj <- runQuery $ do
+        (nm, vd, re, pt, ag, aw) <- query ean getSummaryS
+        imgs <- query ean getImagesS
+        pure $ object [
+            "name" .= nm, "vendor" .= vd, "recyclable" .= re,
+            "images" .= imgs, "plastic_type" .= pt,
+            "average_grade" .= ag, "average_weight" .= aw ]
+    S.json obj
   where
     handleResult = either (S.raise . T.pack . show) pure
-    runQuery ean st = ((lift . run (query ean st)) >=> handleResult) conn
+    --runQuery ean st = ((lift . run (query ean st)) >=> handleResult) conn
+    runQuery = handleResult <=< lift . flip run conn
 
