189539129d96c1add8b21303c98fd83b3648c321
Tobias Markus <tobias@miglix.eu>
2018-05-27 02:36:27 +0200

    PUT /image: image data in body

--- src/PutImage.hs
+++ src/PutImage.hs
@@ -8,6 +8,8 @@ import Hasql.Connection
 import qualified Hasql.Decoders as D
 import Hasql.Query
 
+
+import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text.Lazy as T
 import Data.UUID
 import Data.Maybe
@@ -24,7 +26,7 @@ putImage conn =
   S.put "/product/:productid/image" $ do
     uuid <- lift $ randomIO
     productid <- S.param "productid"
-    imagedata <- S.param "data"
+    imagedata <- BS.toStrict <$> S.body
     let sq = "insert into product_images (id, productid, image_data) values ($1, $2, $3)"
         st = statement sq imageP D.unit True
         im = Image uuid imagedata productid
